263afe8283f200942eef040a50ac0dc5efae0673
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-13 02:33:05 +0600

    feature - delete a single appintment added

--- src/pages/MyAppointments/MyAppointments.jsx
+++ src/pages/MyAppointments/MyAppointments.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
 import AppointmentsTable from './AppointmentsTable';
+import Swal from 'sweetalert2';
 
 const MyAppointments = () => {
     const [appointments, setAppointments] = useState()
@@ -11,6 +12,45 @@ const MyAppointments = () => {
             .then(res => res.json())
             .then(data => setAppointments(data))
     }, [url])
+
+    const handleAppointmentDelete = (id) => {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/appointments/${id}`, {
+
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if(data.deletedCount>1)
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+
+                        const remainingAppointments = appointments.filter(appointment => appointment._id !== id);
+                        setAppointments(remainingAppointments)
+                    })
+                
+            }
+        })
+
+
+
+
+
+    }
 
     return (
         <div className='mt-8 mb-5'>
@@ -32,17 +72,18 @@ const MyAppointments = () => {
                         </tr>
                     </thead>
                     <tbody>
-                       
-                       {
-                        appointments?.map(appointment => <AppointmentsTable
-                        key={appointment._id}
-                        appointment={appointment}
-                        ></AppointmentsTable>)
-                       }
-                        
-                       
+
+                        {
+                            appointments?.map(appointment => <AppointmentsTable
+                                key={appointment._id}
+                                appointment={appointment}
+                                handleAppointmentDelete={handleAppointmentDelete}
+                            ></AppointmentsTable>)
+                        }
+
+
                     </tbody>
-                    
+
 
                 </table>
             </div>
